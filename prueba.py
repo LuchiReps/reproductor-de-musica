ed2fac953b3c269f2a05b9b9dd469c904e0fc154
Your Name <you@example.com>
2024-05-28 00:20:13 -0300

    python

---
+++ prueba.py
@@ -0,0 +1,33 @@
+# print("Hola Mundo")
+# import sys
+
+# print(sys.version)
+# if 5 > 2:
+#   print("Five is greater than two!")
+# x = 5
+# y = "Hello, World!"
+# print(x)
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+# print(x)
+# print(y)
+# print(z)
+# a = 4
+# A = "Sally"
+# print(A)
+# myvar = "John"
+# my_var = "Mariano"
+# _my_var = "Josefina"
+# myVar = "Maria"
+# MYVAR = "Regina"
+# myvar2 = "Rodolfo"
+# print(myvar2)
+# x, y, z = "La Nueva", "La Nueva", "Y la EX"
+# print(x)
+# print(y)
+# print(z)
